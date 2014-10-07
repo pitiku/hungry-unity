e7ae3bcad1910386886919fce4cce0c8d7f8df89
pitiku <pitiku@gmail.com>
2014-10-07 07:30:57 +0000

    stats

--- Assets/Scripts/Menu/OptionsMenu.cs
+++ Assets/Scripts/Menu/OptionsMenu.cs
@@ -5,12 +5,19 @@ public class OptionsMenu : MenuPage
 {
 	public MenuItem musicItem;
 	public SpriteRenderer musicCheckbox;
+	public MenuItem soundItem;
+	public SpriteRenderer soundCheckbox;
+	public MenuItem accelerometerItem;
+	public SpriteRenderer accelerometerCheckbox;
+	public MenuItem statsItem;
 
 	public MenuItem backButton;
 
 	public override void OnStart () 
 	{
 		musicCheckbox.enabled = PlayerData.Instance.option_music;
+		soundCheckbox.enabled = PlayerData.Instance.option_sound;
+		accelerometerCheckbox.enabled = PlayerData.Instance.option_accelerometer;
 	}
 	
 	public override void OnUpdate () 
@@ -19,6 +26,20 @@ public class OptionsMenu : MenuPage
 		{
 			PlayerData.Instance.option_music = !PlayerData.Instance.option_music;
 			musicCheckbox.enabled = PlayerData.Instance.option_music;
+		}
+		else if(soundItem.IsJustPressed())
+		{
+			PlayerData.Instance.option_sound = !PlayerData.Instance.option_sound;
+			soundCheckbox.enabled = PlayerData.Instance.option_sound;
+		}
+		else if(accelerometerItem.IsJustPressed())
+		{
+			PlayerData.Instance.option_accelerometer = !PlayerData.Instance.option_accelerometer;
+			accelerometerCheckbox.enabled = PlayerData.Instance.option_accelerometer;
+		}
+		else if(statsItem.IsJustPressed())
+		{
+			MenuManager.Instance.SetPage(MenuManager.Instance.statsPage);
 		}
 		else if(backButton.IsJustPressed() || Input.GetKeyDown(KeyCode.Escape))
 		{
